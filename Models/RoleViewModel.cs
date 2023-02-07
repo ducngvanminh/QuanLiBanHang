9ffdcd33b15938410a102f3b309bcc05c4c7790d
Draken <trumco6@gmail.com>
2023-02-07 14:44:21 +0700

    files added

---
+++ Models/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace QLBanhang.Models
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name="Role Name")]
+        public string Name { get; set; }
+    }
+}
